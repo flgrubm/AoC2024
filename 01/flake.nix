7fe564d09cc4320aacfdc7cb1c596d19c1b4e94e
Fabian Lukas Grubmüller <fabian.grubmueller@gmail.com>
2024-12-01 20:05:39 +0100

    Add solutions for day 1

---
+++ 01/flake.nix
@@ -0,0 +1,74 @@
+# Copyright © 2024 Fabian Grubmüller
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction,including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+{
+  description = "A flake for developing a Haskell project";
+
+  inputs = {
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    haskell-flake.url = "github:srid/haskell-flake";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = with inputs; [
+        devshell.flakeModule
+        haskell-flake.flakeModule
+        treefmt-nix.flakeModule
+      ];
+
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        {
+          self',
+          ...
+        }:
+        let
+          customDependencies = hp: {
+            # name = package;
+            # hello = pkgs.hello;
+            split = hp.split;
+          };
+
+          dependencies = customDependencies;
+        in
+        {
+          haskellProjects.default.devShell.tools = dependencies;
+          packages.default = self'.packages.project;
+
+          treefmt = {
+            programs = {
+              fourmolu.enable = true;
+              nixfmt.enable = true;
+            };
+            projectRootFile = "flake.nix";
+          };
+        };
+    };
+}
